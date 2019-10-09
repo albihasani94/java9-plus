52fbdf56af8601a1d70cc7be24552f7e048123fd
albihasani94 <albihasani94@gmail.com>
2019-10-09 23:28:59 +0200

    Add just the right amount of comments

--- src/main/java/com/jdk/java9plus/collection/FactoryMethods.java
+++ src/main/java/com/jdk/java9plus/collection/FactoryMethods.java
@@ -9,7 +9,7 @@ public class FactoryMethods {
     public static void main(String[] args) {
 
         List<String> immutableBooks = List.of("Art of War", "Willpower", "Never Split the Difference");
-        System.out.println(immutableBooks.getClass());
+        System.out.println(immutableBooks.getClass()); // java.util.ImmutableCollections$ListN
 
         try {
             immutableBooks.add("Soft Skills");
@@ -18,10 +18,10 @@ public class FactoryMethods {
         }
 
         Set<Integer> immutableIntegers = Set.of(1, 2);
-        System.out.println(immutableIntegers.getClass());
+        System.out.println(immutableIntegers.getClass()); // java.util.ImmutableCollections$Set12
 
         Map<String, Integer> bookRatings = Map.of("Art of War", 6, "Willpower", 8, "Never Split the Difference", 10);
-        System.out.println(bookRatings.getClass());
+        System.out.println(bookRatings.getClass()); // java.util.ImmutableCollections$MapN
 
     }
 
