4630f30b63b3d1739ec80786e6114d92859d8c68
albihasani94 <albihasani94@gmail.com>
2019-10-10 01:08:47 +0200

    Add filtering and flatMapping stream collectors

---
+++ src/main/java/com/jdk/java9plus/stream/NewCollectors.java
@@ -0,0 +1,33 @@
+package com.jdk.java9plus.stream;
+
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.*;
+
+public class NewCollectors {
+
+    public static void main(String[] args) {
+
+        Stream<Book> books = Book.getBooks();
+
+        Map<Set<String>, Set<Book>> booksByAuthors = books.collect(
+                groupingBy(Book::getAuthors, // authors are not filtered :)
+                        filtering(book -> book.getPrice() > 5, // filter books costing more than 5
+                                toSet()))
+        );
+        System.out.println(booksByAuthors);
+
+        books = Book.getBooks();
+
+        Map<Double, Set<String>> authorsSellingForPrice = books.collect(
+                groupingBy(Book::getPrice,
+                        flatMapping(book -> book.getAuthors().stream(), // get all authors who sell at this price
+                                toSet()))
+        );
+        System.out.println(authorsSellingForPrice);
+
+    }
+
+}
